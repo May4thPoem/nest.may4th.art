ca3d4c5ca47be67bbefb09a22e5251abd4f93a7e
Gabriel Wu <qqbbnease1004@126.com>
2019-04-07 18:10:37 +0800

    chore: deploy

--- src/common/schema.ts
+++ src/common/schema.ts
@@ -5,20 +5,25 @@ input CreateUserInput {
   password: String!
 }
 
+scalar DateTime
+
 type Mutation {
   signUp(newUser: CreateUserInput!): Session!
   logIn(password: String!, email: String!): Session!
+  postPoem(content: String!, title: String!, token: String!): Poem!
 }
 
 type Poem {
   id: ID!
   title: String!
   content: String!
+  createdAt: DateTime!
   author: User!
 }
 
 type Query {
   user(id: String!): User!
+  poem(id: String!): Poem!
 }
 
 type Session {
@@ -31,4 +36,5 @@ type User {
   name: String!
   email: String!
   poems: [Poem!]!
+}
 `
